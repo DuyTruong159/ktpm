98f19295081edd4aa130da628f998147ab3f62b5
DuyTruong159 <73580494+DuyTruong159@users.noreply.github.com>
2021-04-09 21:22:12 +0700

    dang lam bang dat ve

--- src/main/java/com/team/btlon/SecondaryController.java
+++ src/main/java/com/team/btlon/SecondaryController.java
@@ -1,12 +1,29 @@
 package com.team.btlon;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
-public class SecondaryController {
-
-    @FXML
-    private void switchToPrimary() throws IOException {
+public class SecondaryController implements Initializable{
+    
+    @FXML private void btBack() throws IOException {
         App.setRoot("primary");
+    }
+    
+    @FXML private RadioButton rbFirstclass;
+    @FXML private RadioButton rdSecondclass;
+    
+    public void radioButton (ActionEvent event) {
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
     }
 }
